6c232c4be7b161b74db1a004eb500a848b730f57
buzz-code <72219643+buzz-code@users.noreply.github.com>
2021-08-10 22:02:40 +0300

    update imports

--- server/controllers/report.controller.js
+++ server/controllers/report.controller.js
@@ -4,7 +4,7 @@ import bookshelf from '../../common-modules/server/config/bookshelf';
 import knex from '../../common-modules/server/config/knex';
 import Report from '../models/report.model';
 import Student from '../models/student.model';
-import { getListFromTable } from '../utils/commonUtils';
+import { getListFromTable } from '../../common-modules/server/utils/common';
 import genericController, { applyFilters, fetchPage } from '../../common-modules/server/controllers/generic.controller';
 
 export const { findById, store, update, destroy, uploadMultiple } = genericController(Report);
